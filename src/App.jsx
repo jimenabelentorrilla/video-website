69091a01aa035aaf2c3672063e0c92a8f11f3d77
Jimena Belen <jiment4@hotmail.com>
2023-04-06 07:34:59 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Sidebar from './components/Sidebar';
+import HeroContainer from './components/HeroContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <Sidebar />
+      <HeroContainer />
+    
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Logo from "../assets/Logo.png";
+import Search from "../assets/search-icon.png";
+import Movie from "../assets/movie-icon.png";
+import Fav from "../assets/fav-icon.png";
+import User from "../assets/movie-icon.png";
+
+const Sidebar = () => {
+  return (
+    <div>
+        <nav className='sidebar-menu'>
+            <div className='sidebar-flex'>
+                <div>
+                    <img src={Logo} />
+                </div>
+                <div>
+                    <a href="/"><img src={Search} /></a>
+                </div>
+                <div>
+                    <a href="/"><img src={Movie} /></a>
+                </div>
+                <div>
+                    <a href="/"><img src={Fav} /></a>
+                </div>
+                <div>
+                    <a href="/"><img src={User} /></a>
+                </div>
+            </div>
+        </nav>
+    </div>
+  )
+}
+
+export default Sidebar
