59590243e85fdabf31a4dbc2de80db8e5475d35b
Jimena Belen <jiment4@hotmail.com>
2023-04-12 07:04:24 -0300

    peliculas nuevas

--- src/components/ImageGrid.jsx
+++ src/components/ImageGrid.jsx
@@ -1,28 +1,21 @@
 import React from 'react'
 
-const ImageGrid = ({ img0 , img1, img2, img3, img4, img5 }) => {
+
+const ImageGrid = ({ images, onClick }) => {
   return (
-    <div className='image-grid-flex'>
-        <div>
-            <img className="img-w" src={img0} /> 
+    <div>
+        {images.map((image,index) => (
+            <div className='image-grid-flex' key={index}>
+                {image.map((img, index) => (
+                    <div key={index} >
+                        <img className="img-w" src={img} /> 
+                    </div>
+            ))}
         </div>
-        <div>
-            <img className="img-w" src={img1} /> 
-        </div>
-        <div>
-            <img className="img-w" src={img2} /> 
-        </div>
-        <div>
-            <img className="img-w" src={img3} /> 
-        </div>
-        <div>
-            <img className="img-w" src={img4} /> 
-        </div>
-        <div>
-            <img className="img-w" src={img5} /> 
-        </div>
+    ))}
     </div>
   )
 }
 
-export default ImageGrid
+
+export default ImageGrid;
---
+++ src/components/const.jsx
@@ -0,0 +1,40 @@
+
+    const movies = [
+        {
+          title: 'Movie 1',
+          director: 'Director 1',
+          genero: "sarasa",
+          year:"sdads",
+          duration: "sadasd",
+          director:"sadad",
+          cast: 'Cast 1',
+          description: 'Description of Movie 1',
+        },
+        {
+          title: 'Movie 2',
+          director: 'Director 2',
+          genero: "sarasa",
+          year:"sdads",
+          duration: "sadasd",
+          director:"sadad",
+          cast: 'Cast 2',
+          description: 'Description of Movie 2',
+        },
+        {
+          title: 'Movie 3',
+          director: 'Director 3',
+          genero: "sarasa",
+          year:"sdads",
+          duration: "sadasd",
+          director:"sadad",
+          cast: 'Cast 3',
+          description: 'Description of Movie 3',
+        }
+        
+      ];
+    
+    
+
+
+
+
